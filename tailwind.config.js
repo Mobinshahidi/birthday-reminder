369bc0c14006d79e5d6a2284270ded61923f7de2
Mobinshahidi <mowbinsh@gmail.com>
2023-11-02 19:27:17 +0330

    add some custom things

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     './src/components/**/*.{jsx,js}'
   ],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow:{
+				"bx":"rgba(0, 0, 0, 0.1) 0px 10px 50px",
+			},
+			backgroundColor:{
+				"pr":"#370aeb", 
+			}
+		},
 	},
 	plugins: [],
 };
