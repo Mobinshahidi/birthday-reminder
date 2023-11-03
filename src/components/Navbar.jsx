5116680e9ffdbb878b78aafd24885c12e09a50f5
Mobinshahidi <mowbinsh@gmail.com>
2023-11-03 18:15:15 +0330

    creating radio navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+const Navbar = () => {
+	return (
+		<div className="fixed z-10 flex flex-col p-8 top-1/2 right-8 transform -translate-y-1/2 overflow-x-hidden">
+			<Link
+				to="first"
+				activeClass="active"
+				spy={true}
+				smooth={true}
+				offset={0}
+				duration={900}
+				className="px-3 "
+			>
+				<input
+					type="radio"
+					value="section1"
+					name="item"
+					className="cursor-pointer"
+				/>
+			</Link>
+			<Link
+				to="second"
+				activeClass="active"
+				spy={true}
+				smooth={true}
+				offset={0}
+				duration={900}
+				className="px-3"
+			>
+				<input
+					type="radio"
+					value="section2"
+					name="item"
+					className="cursor-pointer"
+					id="section2"
+				/>
+			</Link>
+			<Link
+				to="third"
+				activeClass="active"
+				spy={true}
+				smooth={true}
+				offset={0}
+				duration={900}
+				className="px-3"
+			>
+				<input
+					type="radio"
+					value="section3"
+					name="item"
+					className="cursor-pointer"
+					id="section3"
+				/>
+			</Link>
+			<Link
+				to="fourth"
+				activeClass="active"
+				spy={true}
+				smooth={true}
+				offset={0}
+				duration={900}
+				className="px-3"
+			>
+				<input
+					type="radio"
+					value="section4"
+					name="item"
+					className="cursor-pointer"
+					id="section4"
+				/>
+			</Link>
+			<Link
+				to="fifth"
+				activeClass="active"
+				spy={true}
+				smooth={true}
+				offset={0}
+				duration={900}
+				className="px-3"
+			>
+				<input
+					type="radio"
+					value="section5"
+					name="item"
+					className="cursor-pointer"
+					id="section5"
+				/>
+			</Link>
+		</div>
+	);
+};
+
+export default Navbar;
