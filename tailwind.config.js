8f9622f131adb0b2057ccbf123a68901eb258e04
Mobinshahidi <mowbinsh@gmail.com>
2023-11-03 11:32:52 +0330

    custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
 	theme: {
 		extend: {
 			boxShadow:{
-				"bx":"rgba(0, 0, 0, 0.1) 0px 10px 50px",
+				"bxx":"rgba(0, 0, 0, 0.1) 0px 10px 50px",
+				"bx":"rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px"
 			},
 			backgroundColor:{
 				"pr":"#370aeb", 
