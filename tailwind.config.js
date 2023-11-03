73fd7128d7858ec7f822380eab540867373252d6
Mobinshahidi <mowbinsh@gmail.com>
2023-11-03 18:14:42 +0330

    custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-    './**/*.html',
-    './src/components/**/*.{jsx,js}'
-  ],
+	content: ['./**/*.html', './src/components/**/*.{jsx,js}'],
 	theme: {
 		extend: {
-			boxShadow:{
-				"bxx":"rgba(0, 0, 0, 0.1) 0px 10px 50px",
-				"bx":"rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px"
+			boxShadow: {
+				bxx: 'rgba(0, 0, 0, 0.1) 0px 10px 50px',
+				bx: 'rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px',
+				b: 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px',
+				c: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px',
 			},
-			backgroundColor:{
-				"pr":"#370aeb", 
-			}
+			backgroundColor: {
+				pr: '#370aeb',
+				bt: 'rgb(38, 52, 253)',
+				bth:"rgb(67, 79, 249)"
+			},
+			borderColor: {
+				bt: 'rgb(38, 52, 253)',
+				bth:"rgb(67, 79, 249)"
+
+			},
 		},
 	},
 	plugins: [],
