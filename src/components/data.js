a86d573800471f34eb50a9744f97a36d2a01e080
Mobinshahidi <mowbinsh@gmail.com>
2024-06-26 12:23:41 +0330

    feat: addBackend

---
+++ src/components/data.js
@@ -0,0 +1,324 @@
+export const data = [
+	{
+		name: 'mobin shahidi',
+		day: 22,
+		month: 7,
+	},
+	{
+		name: 'noora laghaee',
+		day: 26,
+		month: 2,
+	},
+	{
+		name: 'vafa nobakht',
+		day: 23,
+		month: 8,
+	},
+	{
+		name: 'noora habibi',
+		day: 15,
+		month: 9,
+	},
+	{
+		name: 'mesbah misaghi',
+		day: 11,
+		month: 10,
+	},
+	{
+		//TODO
+
+		name: 'mami',
+		day: 12,
+		month: 8,
+	},
+	{
+		name: 'samim sanaei',
+		day: 3,
+		month: 4,
+	},
+	{
+		name: 'alast abolfazli',
+		day: 2,
+		month: 11,
+	},
+	{
+		//TODO
+
+		name: 'seraj safarian',
+		day: 12,
+		month: 2,
+	},
+	{
+		name: 'helia mokhtari',
+		day: 18,
+		month: 8,
+	},
+	{
+		name: 'hedieh rezai',
+		day: 23,
+		month: 2,
+	},
+	{
+		name: 'eliza habibi',
+		day: 20,
+		month: 12,
+	},
+	{
+		name: 'hamed amini',
+		day: 25,
+		month: 3,
+	},
+	{
+		name: 'shahin sadaghiani',
+		day: 12,
+		month: 1,
+	},
+	{
+		name: 'sorena ranjvar',
+		day: 10,
+		month: 2,
+	},
+	{
+		name: 'faran khani',
+		day: 14,
+		month: 8,
+	},
+	{
+		name: 'tohid rostami',
+		day: 15,
+		month: 1,
+	},
+	{
+		name: 'nahal zahedi',
+		day: 17,
+		month: 10,
+	},
+	{
+		name: 'arshia hedayati',
+		day: 22,
+		month: 4,
+	},
+	{
+		name: 'sepanta sadeghi',
+		day: 7,
+		month: 7,
+	},
+	{
+		name: 'sevda sadeghi',
+		day: 14,
+		month: 7,
+	},
+	{
+		name: 'diba rohipor',
+		day: 24,
+		month: 11,
+	},
+	{
+		name: 'baba',
+		day: 26,
+		month: 7,
+	},
+	{
+		name: 'maman',
+		day: 20,
+		month: 1,
+	},
+	{
+		name: 'bayan',
+		day: 19,
+		month: 7,
+	},
+	{
+		//TODO
+
+		name: 'pedar',
+		day: 30,
+		month: 10,
+	},
+	{
+		name: 'daii',
+		day: 16,
+		month: 7,
+	},
+	{
+		name: 'zandai',
+		day: 26,
+		month: 5,
+	},
+	{
+		name: 'farnam',
+		day: 26,
+		month: 8,
+	},
+	{
+		name: 'baba zia',
+		day: 19,
+		month: 12,
+	},
+	{
+		name: 'khale sanaz',
+		day: 16,
+		month: 5,
+	},
+	{
+		name: 'nafhe sahrai',
+		day: 12,
+		month: 6,
+	},
+	{
+		name: 'farzam shirvani',
+		day: 12,
+		month: 6,
+	},
+	{
+		name: 'mani shirvani',
+		day: 8,
+		month: 7,
+	},
+	{
+		name: 'shirin shirvani',
+		day: 8,
+		month: 7,
+	},
+	{
+		//TODO
+
+		name: 'farinaz hedayati',
+		day: 30,
+		month: 10,
+	},
+	{
+		name: 'naser habibi',
+		day: 30,
+		month: 8,
+	},
+	{
+		name: 'shole movafaghi',
+		day: 16,
+		month: 3,
+	},
+	{
+		name: 'nariman movafaghi',
+		day: 27,
+		month: 9,
+	},
+	{
+		name: 'alireza nikzad',
+		day: 5,
+		month: 11,
+	},
+	{
+		name: 'ghazal allahverdi',
+		day: 18,
+		month: 11,
+	},
+	{
+		name: 'adib rahmani',
+		day: 28,
+		month: 11,
+	},
+	{
+		name: 'arian baghaei',
+		day: 3,
+		month: 3,
+	},
+	{
+		name: 'sarah rostami',
+		day: 4,
+		month: 8,
+	},
+	{
+		name: 'noora abbasi',
+		day: 27,
+		month: 1,
+	},
+	{
+		name: 'baran nobakht',
+		day: 9,
+		month: 2,
+	},
+	{
+		name: 'naeim nobakht',
+		day: 26,
+		month: 8,
+	},
+	{
+		name: 'sarvenaz kabiri',
+		day: 16,
+		month: 9,
+	},
+	{
+		name: 'alireza mashhadi',
+		day: 13,
+		month: 11,
+	},
+	{
+		//TODO
+
+		name: 'mani derakhshan',
+		day: 30,
+		month: 6,
+	},
+	{
+		name: 'basir samimi',
+		day: 7,
+		month: 2,
+	},
+	{
+		name: 'firoz zabihi',
+		day: 13,
+		month: 4,
+	},
+	{
+		//TODO
+
+		name: 'arian saadatmand',
+		day: 30,
+		month: 10,
+	},
+	{
+		//TODO
+
+		name: 'leva shaker',
+		day: 30,
+		month: 10,
+	},
+	{
+		//TODO
+
+		name: 'mahsu salehi',
+		day: 30,
+		month: 10,
+	},
+	{
+		//TODO
+
+		name: 'bita rohani',
+		day: 30,
+		month: 10,
+	},
+	{
+		//TODO
+
+		name: 'anahita ettehadi',
+		day: 30,
+		month: 10,
+	},
+	{
+		//TODO
+
+		name: 'parham fallah',
+		day: 30,
+		month: 10,
+	},
+	{
+		name: 'parto ataean',
+		day: 31,
+		month: 2,
+	},
+	{
+		name: 'kati ghasabian',
+		day: 5,
+		month: 3,
+	},
+];
